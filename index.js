33cb26bc6b4fe6f79ff467825e956dd626051329
RZ <skywine13@gmail.com>
2023-06-26 11:01:10 +0800

    feat: move browser DOM library in.

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-import Cache from './node/Cache.js';
-import Is from './Is.js';
-import Log from './Log.js';
-import Riot4Compile from './node/Riot4Compile.js';
-import RiotHttp from './node/RiotHttp.js';
-import RiotMixin from './RiotMixin.js';
-import SQLite from './node/SQLite.js';
-
-export { Cache } from './node/Cache.js';
 export { Is } from './Is.js';
 export { Log } from './Log.js';
+export { RiotMixin } from './RiotMixin.js';
+import Is from './Is.js';
+import Log from './Log.js';
+import RiotMixin from './RiotMixin.js';
+
+export { Cache } from './node/Cache.js';
 export { Riot4Compile } from './node/Riot4Compile.js';
 export { RiotHttp } from './node/RiotHttp.js';
-export { RiotMixin } from './RiotMixin.js';
 export { SQLite } from './node/SQLite.js';
+import Cache from './node/Cache.js';
+import Riot4Compile from './node/Riot4Compile.js';
+import RiotHttp from './node/RiotHttp.js';
+import SQLite from './node/SQLite.js';
+
+export { DOM } from './browser/DOM.js';
+import DOM from './browser/DOM.js';
 
 export default {
   Cache,
+  DOM,
   Is,
   Log,
   Riot4Compile,
   RiotHttp,
   RiotMixin,
-  SQLite
+  SQLite,
 };
