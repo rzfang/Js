e04298aa327ada57e04165e2da70b37c2ba5f3f9
RZ <skywine13@gmail.com>
2023-08-17 15:45:22 +0800

    chore: with riot-4-fun@0.1.2, retire duplicate files.

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 export { Is } from './Is.js';
 export { Log } from './Log.js';
-export { RiotMixin } from './RiotMixin.js';
 import Is from './Is.js';
 import Log from './Log.js';
-import RiotMixin from './RiotMixin.js';
 
 export { Cache } from './node/Cache.js';
-export { Riot4Compile } from './node/Riot4Compile.js';
-export { RiotHttp } from './node/RiotHttp.js';
 export { SQLite } from './node/SQLite.js';
 import Cache from './node/Cache.js';
-import Riot4Compile from './node/Riot4Compile.js';
-import RiotHttp from './node/RiotHttp.js';
 import SQLite from './node/SQLite.js';
 
-export { DOM } from './browser/DOM.js';
 import DOM from './browser/DOM.js';
 
 export default {
@@ -22,8 +15,5 @@ export default {
   DOM,
   Is,
   Log,
-  Riot4Compile,
-  RiotHttp,
-  RiotMixin,
   SQLite,
 };
