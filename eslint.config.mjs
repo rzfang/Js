0864ecec6e8b02be0415aaee7afa511fed58b675
RZ <skywine13@gmail.com>
2025-10-23 17:41:11 +0800

    chore: enable eslint. chore: v0.6.0

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import stylistic from '@stylistic/eslint-plugin'
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: [ '**/*.{js,mjs,cjs}' ],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, jQuery: true },
+    },
+  },
+  {
+    extends: [ 'json/recommended' ],
+    files: [ '**/*.json' ],
+    ignores: [ 'package-lock.json' ],
+    language: 'json/json',
+    plugins: { json },
+  },
+  {
+    extends: [ 'markdown/recommended' ],
+    files: [ '**/*.md' ],
+    language: 'markdown/gfm',
+    plugins: { markdown },
+  },
+  {
+    extends: [ 'js/recommended' ],
+    files: [ '**/*.{js,mjs,cjs}' ],
+    ignores: [ 'DST/*' ],
+    plugins: { '@stylistic': stylistic, js },
+    rules: {
+      '@stylistic/array-bracket-spacing': [ 'error', 'always' ],
+      '@stylistic/dot-location': [ 'error', 'property' ],
+      '@stylistic/indent': [ 'error', 2 ],
+      '@stylistic/max-len': [ 'error', { code: 120, ignoreComments: true } ],
+      '@stylistic/quote-props': [ 'error', 'as-needed' ],
+      '@stylistic/quotes': [ 'error', 'single', { avoidEscape: true, allowTemplateLiterals: 'avoidEscape' } ],
+      'no-console': [ 'warn' ],
+      'no-unused-vars': [ 'warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' } ],
+      'prefer-const': [ 'error' ],
+      '@stylistic/comma-dangle': [
+        'error',
+        {
+          arrays: 'always-multiline',
+          exports: 'always-multiline',
+          functions: 'never',
+          imports: 'always-multiline',
+          objects: 'always-multiline',
+        },
+      ],
+    },
+  },
+]);
