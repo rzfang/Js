eeff8bc5fcbfbfb315106539597cd4ff9226c893
RZ <skywine13@gmail.com>
2025-10-25 01:29:25 +0800

    fix: interface of export.

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ import is from './is.mjs';
 import log from './log.mjs';
 import sqlite from './node/sqlite.mjs';
 
+export {
+  cache,
+  cache as Cache, // ToDo: deprecated. the alias.
+  dom,
+  dom as DOM, // ToDo: deprecated. the alias.
+  is,
+  is as Is, // ToDo: deprecated. the alias.
+  log,
+  log as Log, // ToDo: deprecated. the alias.
+  sqlite,
+  sqlite as SQLite, // ToDo: deprecated. the alias.
+};
+
 export default {
   cache,
   Cache: cache, // ToDo: deprecated. the alias.
